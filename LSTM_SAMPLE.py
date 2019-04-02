70c58a39cc8d9c93e1f674c3328b181b17027140
Safvan <safvan.ck@iinerds.com>
2019-04-02 23:07:01 +0530

    pep8 standard

--- LSTM_SAMPLE.py
+++ LSTM_SAMPLE.py
@@ -1,18 +1,18 @@
+import string
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, LSTM
+import pandas as pd
+from numpy import array
 from keras import optimizers
+import matplotlib.pyplot as plt
+from dateutil.parser import parse
+from keras.models import Sequential
+from keras.models import load_model
+from keras.layers import Dense, Dropout, LSTM
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
-from dateutil.parser import parse
-import matplotlib.pyplot as plt
-import string
-from numpy import array
-from keras.models import load_model
-import pandas as pd
 
-
+#creating a sequence
 length = 100
 seq = array([i / 100 for i in range(length)])
 print(seq)
@@ -23,7 +23,7 @@ y = seq[1:]
 y = y.reshape(len(y), 1)
 
 
-n_epoch = 200
+n_epoch = 1000
 # create LSTM
 model = Sequential()
 model.add(LSTM(1000, input_shape=(1, 1)))
@@ -35,4 +35,5 @@ print(model.summary())
 model.fit(X, y, epochs=n_epoch, batch_size=32,
           validation_split=0.2, shuffle=True)
 
+#predict
 model.predict(X[0:10])
